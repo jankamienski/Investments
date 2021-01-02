6689dcd04ad6e77011c019077842ad8d43a0dc4a
jankamienski <kamienski.jan@gmail.com>
2021-01-02 22:16:53 +0100

    Start making RWD, change script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,8 @@
 const sliderButton = document.querySelector('.hamburger');
-const sliderMenu = document.querySelector('#sidebar-menu');
+const sliderMenu = document.querySelector('body');
 
 sliderButton.addEventListener('click', function(event) {
   event.preventDefault();
   sliderMenu.classList.toggle('active');
+  //topper.classList.toggle('hide');
 });
